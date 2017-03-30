6930273dc0e8b5d25164f6b72188d1da272f7471
Arshiya <arshfrndz.af@gmail.com>
2017-03-30 18:17:11 +0530

    sop statement added

--- src/main/java/com/airdit/Test.java
+++ src/main/java/com/airdit/Test.java
@@ -3,6 +3,7 @@ package com.airdit;
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("last commit");
 		System.out.println("first commit");
 		int a = 10;
 		System.out.println("second commit:"+a);
